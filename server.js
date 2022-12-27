d3cf7b8051024684bca33855d6f6fbc23a763726
Piyush Priyam <pr16866@gmail.com>
2022-12-27 21:21:37 +0530

    updated

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use(express.static("images/"));
 if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
   //  app.use(express.static(path.join(__dirname,"/client/build")));
-  // app.get("*", (req, res) => {
-  //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("piyush");
